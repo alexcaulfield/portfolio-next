77981673f1b80b5503628616e6d96fe866438ad8
Alex Caulfield <alex.caulfield28@gmail.com>
2022-02-27 14:31:46 -0500

    fixing mweb

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { getBlocks } from "../lib/notion"
 import NotionBlock, {BlockData} from '../components/NotionBlock'
-import { Box, IconButton, Flex, Link, Container } from '@chakra-ui/react'
+import { Box, IconButton, Flex, Link, Center } from '@chakra-ui/react'
 import { FaLinkedin, FaGithubSquare } from 'react-icons/fa'
 import Fuji from '../resources/images/fuji.jpg'
 import Navbar from '../components/Navbar';
@@ -16,53 +16,55 @@ interface Props {
 }
 const Home = ({data}: Props) => {
   return (
-    <Container maxWidth='1600px'>
-      <div className={styles.container}>
-        <Head>
-          <title>Alex Caulfield</title>
-          <meta name="description" content="Alex Caulfield's personal website" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <Center>
+      <Box maxWidth={{ base: '100%', xl: '1600px' }}>
+        <div className={styles.container}>
+          <Head>
+            <title>Alex Caulfield</title>
+            <meta name="description" content="Alex Caulfield's personal website" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        <Navbar />
-        <main className={styles.main}>
-          <Box p={4} display={{ md: 'flex' }}>
-            <Box flexShrink={0}>
-              <Image 
-                width={600}
-                height={400}
-                src={Fuji}
-                alt='Mt Fuji'
-                placeholder='blur'
-              /> 
+          <Navbar />
+          <main className={styles.main}>
+            <Box p={4} display={{ md: 'flex' }}>
+              <Box flexShrink={0}>
+                <Image 
+                  width={600}
+                  height={400}
+                  src={Fuji}
+                  alt='Mt Fuji'
+                  placeholder='blur'
+                /> 
+              </Box>
+              <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+                {data.map(block => (
+                  <Box key={block.id} mb={4}>
+                    <NotionBlock {...block} />
+                  </Box>
+                ))}
+                <Flex justify='center'>
+                  <Link href='https://linkedin.com/in/alexandercaulfield' isExternal>
+                    <IconButton
+                      aria-label='LinkedIn Profile'
+                      as={FaLinkedin}
+                      mx={2}
+                    />
+                  </Link>
+                  <Link href='https://github.com/alexcaulfield' isExternal>
+                    <IconButton
+                      aria-label='Github Profile'
+                      as={FaGithubSquare}
+                      mx={2}
+                    />
+                  </Link>
+                </Flex>
+              </Box>
             </Box>
-            <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
-              {data.map(block => (
-                <Box key={block.id} mb={4}>
-                  <NotionBlock {...block} />
-                </Box>
-              ))}
-              <Flex justify='center'>
-                <Link href='https://linkedin.com/in/alexandercaulfield' isExternal>
-                  <IconButton
-                    aria-label='LinkedIn Profile'
-                    as={FaLinkedin}
-                    mx={2}
-                  />
-                </Link>
-                <Link href='https://github.com/alexcaulfield' isExternal>
-                  <IconButton
-                    aria-label='Github Profile'
-                    as={FaGithubSquare}
-                    mx={2}
-                  />
-                </Link>
-              </Flex>
-            </Box>
-          </Box>
-        </main>
-      </div>
-    </Container>
+          </main>
+        </div>
+      </Box>
+    </Center>
   )
 }
 
